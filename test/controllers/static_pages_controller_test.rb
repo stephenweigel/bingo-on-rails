ec6bea4c41ee52ec740530ee26117ef2bb940c2f
Stephen Weigel <stephen@stephenweigel.com>
2015-08-17 10:19:47 -0400

    Setup Home, About, and Help pages with routes and tests for page titles
    Add full_title method in ApplicationHelper for dynamic page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Bingo on Rails"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Bingo on Rails"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Bingo on Rails"
   end
 
 end
