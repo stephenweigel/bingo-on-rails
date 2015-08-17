d7cb3a6760f95959e93d91b4120888153b46f109
Stephen Weigel <stephen@stephenweigel.com>
2015-08-17 11:22:09 -0400

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'help'  =>  'static_pages#help'
-  get 'about' =>  'static_pages#about'
+  get 'help'  	=>  'static_pages#help'
+  get 'about' 	=>  'static_pages#about'
+  get 'signup' 	=>	'users#new'
 end
